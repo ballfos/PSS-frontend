190b4d24674b3aef2a014f93a8b8cc93577b9442
garden905 <balboa.0905@gmail.com>
2025-05-31 14:28:56 +0900

    Home画面の背景色を変更
    Co-authored-by: gakkun_m1 <gakkungithub@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,13 @@ import "./Home.css";
 function Home() {
   const [isPresence, setIsPresence] = useState<boolean>(false);
   return (
-    <div>
-        <button className={`button-presence ${isPresence ? 'presence' : 'notpresence'}`} onClick={() => setIsPresence(!isPresence)}>
-          {isPresence ? "退室する" : "入室する"}
-        </button>
+    <div className="Home">
+      <button
+        className={`button-presence ${isPresence ? "presence" : "notpresence"}`}
+        onClick={() => setIsPresence(!isPresence)}
+      >
+        {isPresence ? "退室する" : "入室する"}
+      </button>
     </div>
   );
 }
