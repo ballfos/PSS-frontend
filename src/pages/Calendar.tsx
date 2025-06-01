52c22fd7670b2b6fe2214d2b2a78f2444818e3a4
garden905 <balboa.0905@gmail.com>
2025-06-01 12:37:47 +0900

    カレンダーを作成

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import "./Calendar.css";
-function Calendar() {
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import choi_icon from "../assets/choi_icon.png";
+
+function CalendarPage() {
+  const isToday = (date: Date) => {
+    const today = new Date();
+    return (
+      date.getFullYear() === today.getFullYear() &&
+      date.getMonth() === today.getMonth() &&
+      date.getDate() === today.getDate()
+    );
+  };
+
   return (
     <div className="calendar">
       <h1>カレンダーページ</h1>
-      <p>ここはカレンダーページです。</p>
+      <Calendar
+        tileContent={({ date, view }) =>
+          view === "month" && isToday(date) ? (
+            <div className="overlay-container">
+              <img src={choi_icon} alt="today" className="icon-image" />
+              <span className="date-text">{date.getDate()}</span>
+            </div>
+          ) : null
+        }
+        tileClassName={({ date, view }) => {
+          if (isToday(date)) {
+            return "today";
+          }
+          if (view === "month" && date.getDay() === 6) {
+            return "saturday";
+          }
+          return null;
+        }}
+      />
     </div>
   );
 }
 
-export default Calendar;
+export default CalendarPage;
