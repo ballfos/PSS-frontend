466cc695cf96b583766255d5b157b403b76ea07f
garden905 <balboa.0905@gmail.com>
2025-06-01 10:47:49 +0900

    ButtonsFooterをコンポーネント化

---
+++ src/components/ButtonsFooter.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+import "./ButtonsFooter.css";
+
+function ButtonsFooter() {
+  const isCurrent = ({
+    isActive,
+    isPending,
+  }: {
+    isActive: boolean;
+    isPending: boolean;
+  }) => (isActive ? "linkButton activeLink" : "linkButton");
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/Home" className={isCurrent}>
+            ホーム
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/calendar" className={isCurrent}>
+            カレンダー
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/settings" className={isCurrent}>
+            設定
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default ButtonsFooter;
